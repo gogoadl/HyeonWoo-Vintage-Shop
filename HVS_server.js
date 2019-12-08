364fa467686bc506df97c9a5bcf1a36acc1efc08
gogoadl <49335446+gogoadl@users.noreply.github.com>
2019-12-08 22:48:55 +0900

    version 1.0
    - DB 연동
    - UI 설계

---
+++ HVS_server.js
@@ -0,0 +1,160 @@
+var express = require('express'); // 경량 http 웹 프레임워크 요청과 응답, 미들웨어 사용, 템플릿 엔진을 제공한다
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+// var hostname = '203.232.193.175';
+// var portname = '52273';
+
+// 데이터베이스 연결
+var client = mysql.createConnection({
+    user: 'root',
+    password: '1234',
+    database: 'HVS_db'
+});
+
+var app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(52273, function () { // 로컬말고 
+    console.log('Server Running at http://127.0.0.1:52273');
+});
+
+// app.listen(portname, hostname, function () { // 로컬말고 
+//     console.log('Server Running at http://'+hostname+':'+portname);
+// });
+
+//------------------------- 사용자 DB 접근 -------------------------- 
+
+app.post('/login', function (request, response) {
+    // 데이터베이스 요청을 수행합니다.
+
+    id = request.body.id;
+    pw = request.body.pw;
+
+    console.log("사용자 아이디 : " + request.body.id);
+    console.log("사용자 비밀번호 : " + request.body.pw);
+
+    //var _query = "select * from hvs_member where ID='?' and PASSWORD='?'",[id,pw];
+    
+    client.query('SELECT * FROM HVS_MEMBER WHERE ID=? and PASSWORD=?;',[id,pw]
+        , function (error, data) {
+            if(error)
+            {
+                console.log("에러 발생" + error);
+            }
+            else
+            {
+                
+            }
+
+        console.log(data);
+        response.send(data);
+        
+    });
+  });
+
+  app.post('/signup', function (request, response) {
+    // 데이터베이스 요청을 수행합니다.
+
+    id = request.body.id;
+    pw = request.body.pw;
+    name = request.body.name;
+    phone = request.body.phone;
+
+    console.log("사용자 아이디 : " + request.body.id);
+    console.log("사용자 비밀번호 : " + request.body.pw);
+    console.log("사용자 이름 : " + request.body.name);
+    console.log("사용자 휴대전화 : " + request.body.phone);
+
+    client.query('INSERT INTO HVS_MEMBER(ID,PASSWORD,NAME,PHONE) VALUES(?,?,?,?);',[id,pw,name,phone]
+        , function (error, data) {
+            if(error)
+            {
+                console.log("에러 발생" + error);
+                
+            }
+            else
+            {
+                console.log(data);
+                response.send(data);
+            }     
+        
+    });
+  });
+
+//------------------------- 상품 DB 접근 ----------------------------
+
+app.get('/products', function (request, response) {
+  // 데이터베이스 요청을 수행합니다.
+  client.query('SELECT * FROM HVS_products', function (error, data) {
+      response.send(data);
+  });
+});
+
+app.get('/products/:id', function (request, response) {
+    // 변수를 선언합니다.
+    var id = Number(request.params.id);
+
+    // 데이터베이스 요청을 수행합니다.
+    client.query('SELECT * FROM hvs_products WHERE product_number=?', [
+        id
+    ], function (error, data) {
+        response.send(data);
+    })
+});
+
+app.post('/products', function (request, response) {
+    // 변수를 선언합니다.
+    var NAME = request.body.PRODUCT_NAME;
+    var CATEGORY = request.body.PRODUCT_CATEGORY;
+    var IMG =  request.body.PRODUCT_IMG;
+    var PRICE = request.body.PRODUCT_PRICE;
+    var INFO = request.body.PRODUCT_INFO;
+
+    // 데이터베이스 요청을 수행합니다.
+    client.query('INSERT INTO hvs_products(PRODUCT_NAME,PRODUCT_IMG,PRODUCT_PRICE,PRODUCT_INFO,PRODUCT_CATEGORY) VALUES(?,?,?,?,?)', 
+       [NAME, IMG, PRICE, INFO, CATEGORY], function (error, data) {
+           if(error)
+           {
+               response.send(error);
+               console.log("상품 등록에 실패 했습니다.");
+           }
+           else
+           {
+            response.send(data);
+            console.log("DB에 데이터 삽입 성공");
+           }
+           
+    });
+});
+
+app.put('/products/:id', function (request, response) {
+    // 변수를 선언합니다.
+    var id = Number(request.body.id);
+    var NAME = request.body.PRODUCT_NAME;
+    var IMG = request.body.PRODUCT_IMG;
+    var PRICE = request.body.PRODUCT_PRICE;
+    var INFO = request.body.PRODUCT_INFO;
+    var query = 'UPDATE hvs_products SET ';
+    // 쿼리를 생성합니다.
+    if (NAME) query += 'product_name="' + name + '" ';
+    if (IMG) query += 'product_img="' + IMG + '" ';
+    if (PRICE) query += 'product_price="' + modelnumber + '" ';
+    if (INFO) query += 'product_info="' + series + '" ';
+    query += 'WHERE id=' + id;
+    // 데이터베이스 요청을 수행합니다.
+    client.query(query, function (error, data) {
+        response.send(data);
+    });
+});
+
+app.delete('/products/:id', function (request, response) {
+    // 변수를 선언합니다.
+    var id = Number(request.params.id);
+
+    // 데이터베이스 요청을 수행합니다.
+    client.query('DELETE FROM hvs_products WHERE id=', [id], function (error, data) {
+        response.send(data);
+    });
+});
